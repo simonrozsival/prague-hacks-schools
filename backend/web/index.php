be306fa17061c93e5bf9844ae161c009f46a4d12
Tomáš Fejfar <tomas.fejfar@gmail.com>
2015-06-13 13:59:05 +0200

    Updated backend URL

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,23 +1,20 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 define('ROOT', realpath(__DIR__ . '/../'));
-use Symfony\Component\HttpFoundation\Response as Response;
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-
-$app->get('/', function () use ($app) {
+$app->get('/api', function () use ($app) {
     return $app->json(['msg' => 'Hello, world!']);
 });
 
-$app->get('/backend', function () use ($app) {
-    return $app['twig']->render('index/index.twig', array(
-    ));
-}) ;
+$app->get('/backend/', function () use ($app) {
+    return $app['twig']->render('index/index.twig', []);
+});
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => ROOT.'/app/views',
+    'twig.path' => ROOT . '/app/views',
 ));
 
 $app->run();
