706e24048c198e9ffddc8d4d11c415316e6c4dea
Tomáš Fejfar <tomas.fejfar@gmail.com>
2015-06-13 13:38:07 +0200

    Twig layout + demo view

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,15 +1,23 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
-use Symfony\Component\HttpFoundation\Request;
+define('ROOT', realpath(__DIR__ . '/../'));
+use Symfony\Component\HttpFoundation\Response as Response;
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 
-$app->get('/', function () {
-    return '{
-  "msg": "Hello, world!"
-}';
+$app->get('/', function () use ($app) {
+    return $app->json(['msg' => 'Hello, world!']);
 });
+
+$app->get('/backend', function () use ($app) {
+    return $app['twig']->render('index/index.twig', array(
+    ));
+}) ;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => ROOT.'/app/views',
+));
 
 $app->run();
