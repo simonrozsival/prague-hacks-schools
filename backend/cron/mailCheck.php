e15b3dbd86d4f66ba0464f2802eed3902aff36d0
Tomáš Fejfar <tomas.fejfar@gmail.com>
2015-06-14 16:23:20 +0200

    Cleanup ConfirmationEmail

--- backend/cron/mailCheck.php
+++ backend/cron/mailCheck.php
@@ -5,4 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 * skript pro zarazeni do cronu a pravidelne (v radu minut) spousteni
 */
 require_once '../src/ConfirmationEmail.php';
-checkMails('prague.hacks.schools@gmail.com', 'prague-hacks');
+if (checkMails('prague.hacks.schools@gmail.com', 'prague-hacks')) {
+    echo 'OK';
+} else {
+    echo 'KO';
+}
