c39e8f2fb1f50e8690e2d1a71463b7a6e533cfa1
strakator <jan.kasparek@gmail.com>
2015-06-14 16:01:16 +0200

    mailova sablona

--- backend/cron/mailCheck.php
+++ backend/cron/mailCheck.php
@@ -5,4 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 namespace Hacks\ConfirmationEmail;
 
+/*
+* skript pro zarazeni do cronu a pravidelne (v radu minut) spousteni
+*/
+
 \Hacks\checkMails('prague.hacks.schools@gmail.com', 'prague-hacks');
--- backend/src/ConfirmationEmail.php
+++ backend/src/ConfirmationEmail.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use PDO;
 
-$limit_tokenu = 30; // minuty
+$limit_tokenu = 240; // minuty
 
 class Database {
 
@@ -24,8 +24,8 @@ class Database {
 
 class ConfirmationEmail {
 
-    const ADRESA = 'http://www.example.com/confirm.php?token=';  // TODO vyplnit skutecnou adresu pro potvrzeni tokenem
-    const FROM_EMAIL = 'admin@praguehacks.cz';
+    const ADRESA = 'http://www.example.com/ConfirmationEmail.php?token=';  // TODO vyplnit skutecnou adresu pro potvrzeni tokenem
+    const FROM_EMAIL = 'prague.hacks.schools@google.com';
 
     public $to;
     public $edate;
@@ -43,8 +43,7 @@ class ConfirmationEmail {
 
         $this->body = 'Dobrý den,<br>z vašeho emailu jsme obrželi žádost o rozeslaní zprávy s předmětem "' . $this->subject_encoded .
             '". Pokud chcete emaily opravdu rozeslat, klikněte na <a href="' . self::ADRESA . $this->token . '&email='.$this->to.
-            '">tento link</a>. '.
-        '<br><br>SchoolIn';
+            '" target="_blank">tento link</a> pro potvrzení.<br><br>SchoolIn';
         if ($test) {
             echo $this->body;
         } else {
@@ -62,14 +61,14 @@ class ConfirmationEmail {
 
     public function setSubject($sub) {
         $this->subject = $sub;
-        $s = '';
 
+        $s = '';
         $elements = imap_mime_header_decode($sub);
-        //print_r($elements);
+
         for ($i=0; $i<count($elements); $i++) {
             $s .= mb_convert_encoding($elements[$i]->text,
-                                      'UTF-8',
-                                      ($elements[$i]->charset == 'default' ? 'auto' : $elements[$i]->charset));
+                                      'UTF-8', // from
+                                      ($elements[$i]->charset == 'default' ? 'auto' : $elements[$i]->charset)); // to
         }
         $this->subject_encoded = $s;
 
@@ -89,6 +88,16 @@ class ConfirmationEmail {
 
         return true;
     }
+
+    public function checkSenderEmail() {
+        $db = new Database();
+        $dbh = $db->connect();
+
+        $sql = $dbh->prepare("select * from owner where approved = 1 and email = ?");
+        $sql->execute(array($this->to));
+
+        return ($sql->rowCount() == 1);
+    }
 }
 
 function checkMails($user, $pass)
@@ -104,19 +113,58 @@ function checkMails($user, $pass)
 
         $confirmEmail = new ConfirmationEmail();
         $confirmEmail->to = $mail_header->from[0]->mailbox . '@' . $mail_header->from[0]->host;
-        $confirmEmail->edate = $mail_header->Date;
-        $confirmEmail->setSubject($mail_header->subject);
-        $confirmEmail->send();
 
-        imap_setflag_full($mbox, "$id", "\\Seen");
+        if ( $confirmEmail->checkSenderEmail() ) {
+
+            $confirmEmail->edate = $mail_header->Date;
+            $confirmEmail->setSubject($mail_header->subject);
+            $confirmEmail->send();
+
+            imap_setflag_full($mbox, "$id", "\\Seen");
+
+        } else {
+
+            imap_delete($mbox, $id);
+
+        }
     }
 
+    imap_expunge($mbox);
     imap_close($mbox);
 
     return true;
 }
 
+function getSubscribersEmails($email) {
+    $db = new Database();
+    $dbh = $db->connect();
+
+    $sql = $dbh->prepare("select * from owner o
+                          left join subscriptions s on s.school_id = o.school_id
+                          where o.email = :email and approved = 1");
+    $sql->bindParam(':email', $email, PDO::PARAM_STR);
+    $sql->execute();
+
+    $bcc = [];
+    while ($res = $sql->fetch(PDO::FETCH_ASSOC)) {
+        $bcc[] = $res['email'];
+    }
+
+    return $bcc;
+}
+
 function checkToken($token, $email, $limit) {
+
+    function naplnSablonu($body) {
+        $head = '<!doctype html><html lang="cs"><head><meta charset="UTF-8"><title>Email od naseskola.cz</title></head><body>';
+
+        $structured_body = $body; // TODO dle navrhu vytvorit sablonu a vlozit do toho text z poslaneho emailu
+
+        $tail = '</body></html>';
+
+        return $head . $structured_body . $tail;
+    }
+
     $db = new Database();
     $dbh = $db->connect();
 
@@ -141,17 +189,15 @@ function checkToken($token, $email, $limit) {
                 $from = $headers->from[0]->mailbox . '@' . $headers->from[0]->host;
                 $subject = $headers->subject;
 
-                //$structure = imap_fetchstructure($mbox, $id);
-
                 $raw_body = imap_body($mbox, $id);
 
                 $mail = new \Zend_Mail('utf-8');
                 $mail->setFrom($from);
-                $mail->addTo('neco@example.com');
-//                $mail->addBcc( getMails($res[0]['email_from']) );
+                $bcc = getSubscribersEmails($res[0]['email_from']);
+                $mail->addBcc( $bcc );
                 $mail->setSubject($subject);
-                $mail->setBodyText($raw_body);
-                //$mail->send();
+                $mail->setBodyHtml( naplnSablonu($raw_body) );
+                //if (count($bcc)>0) $mail->send();
 
                 //imap_delete($mbox, $id);
             }
@@ -169,7 +215,7 @@ if (IsSet($_GET['token']) && IsSet($_GET['email'])) {
     checkToken($token, $email, $limit_tokenu);
 }
 
-checkMails('prague.hacks.schools','prague-hacks');
+//checkMails('prague.hacks.schools','prague-hacks');
 
-checkToken('1fe1d2bc7da8591ff2f9cd990b3cbee63eba4782483f8868073acd73a9de23c5', 'jan.kasparek@gmail.com', 240);
-checkToken('dec9c291a348ab95a9eda3c755e9b24bba14e19aaf15e1b0b855388510d436c0', 'no-reply@accounts.google.com', 240);
+/*checkToken('2ef240346fb723db37a8dc48cddb495aacaea1b4c58e6f6cc4800151f5f350ed', 'jan.kasparek@gmail.com', 240);
+checkToken('23045972f8a1d296a71787e1bdf396c3b6b4bc0806be7819389224d3271a624c', 'no-reply@accounts.google.com', 240);*/
