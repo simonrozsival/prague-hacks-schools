f9d17f96c2a471bb8259c7b9b7dd88dd73b53d20
Tomáš Fejfar <tomas.fejfar@gmail.com>
2015-06-14 11:46:01 +0200

    Improved config

--- backend/app/config.schools-hacks.cloudapp.net.php
+++ backend/app/config.schools-hacks.cloudapp.net.php
@@ -4,4 +4,4 @@ $app['elastic.port'] = 8080;
 $app['db.username'] = 'root';
 $app['db.password'] = 'LoremIpsum1';
 $app['db.host'] = '127.0.0.1';
-$app['db.dbname'] = 'praguehacks';
+$app['db.dbname'] = 'schoolin';
