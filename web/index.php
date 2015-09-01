6c4ead7a77529866d6c9a9b4a15854c7207f3267
Tomáš Fejfar <tomas.fejfar@gmail.com>
2015-09-01 10:48:04 +0200

    display errors

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ use Nette\Utils\Json;
 date_default_timezone_set('Europe/London');
 define('ROOT_PATH', realpath(__DIR__ . '/../'));
 require_once __DIR__ . '/../vendor/autoload.php';
-
+ini_set('display_errors', 'on');
 
 $app = new Silex\Application();
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
